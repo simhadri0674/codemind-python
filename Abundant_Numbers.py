b21afad95281fd533efa32ea0f5b8b0b934b46f8
Simhadri surisetti <109957109+simhadri0674@users.noreply.github.com>
2022-10-16 16:07:59 +0530

    a=int(input()) c=0 for i in range(1,a):     if a%i==0:         c +=i if c >a:     print("True") else:     print("False")

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,11 +1,11 @@
 a=int(input())
-b=1
 c=0
-for i in range(b,a):
+for i in range(1,a):
     if a%i==0:
         c +=i
-#print(c)       
-if (c>a):
-   print("True")
+if c >a:
+    print("True")
 else:
     print("False")
+        
+        
