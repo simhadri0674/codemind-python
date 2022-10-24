d16c1f4c69359015e918954458cbed79d1fa368b
Simhadri surisetti <109957109+simhadri0674@users.noreply.github.com>
2022-10-24 17:49:12 +0530

    def prime(a):     if a==0 or a==1:         return 0     for i in range(2,int(a**0.5)+1):         if a%i==0:             return 0     return 1 a=int(input()) l=list(map(int,input().split())) b=int(input()) c=0 for i in l:     if i>=b and prime(i):         c +=1 print(c)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,15 @@
+def prime(a):
+    if a==0 or a==1:
+        return 0
+    for i in range(2,int(a**0.5)+1):
+        if a%i==0:
+            return 0
+    return 1
+a=int(input())
+l=list(map(int,input().split()))
+b=int(input())
+c=0
+for i in l:
+    if i>=b and prime(i):
+        c +=1
+print(c)
