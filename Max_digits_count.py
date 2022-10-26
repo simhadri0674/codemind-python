c134f8c1f8d610c216f7cd49883d92424f63f0c5
Simhadri surisetti <109957109+simhadri0674@users.noreply.github.com>
2022-10-26 07:23:45 +0530

    def count(a):     b=e=0     c=f=0     d=0     if a<=0:         b=a*-1         c=str(b)         d=len(c)         return d     else:         e=str(a)         f=len(e)         return f a=int(input()) l=list(map(int,input().split())) d=[] e=0 for i in l:     j=count(i)     d.append(j) c=max(d) for i in l:     if count(i)==c:         e +=1 print(e)

---
+++ Max_digits_count.py
@@ -0,0 +1,26 @@
+def count(a):
+    b=e=0
+    c=f=0
+    d=0
+    if a<=0:
+        b=a*-1
+        c=str(b)
+        d=len(c)
+        return d
+    else:
+        e=str(a)
+        f=len(e)
+        return f
+a=int(input())
+l=list(map(int,input().split()))
+d=[]
+e=0
+for i in l:
+    j=count(i)
+    d.append(j)
+c=max(d)
+for i in l:
+    if count(i)==c:
+        e +=1
+print(e)
+        
