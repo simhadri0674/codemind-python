ab0d9c063c13b3c861bde79e0d4c959e8a0d7425
Simhadri surisetti <109957109+simhadri0674@users.noreply.github.com>
2022-11-01 21:50:23 +0530

    a=int(input()) d={1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H",9:"I",10:"J",11:"K"} for i in range(a,0,-1):     for j in range(1,i+1):         print(d[i],end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,6 @@
 a=int(input())
+d={1:"A",2:"B",3:"C",4:"D",5:"E",6:"F",7:"G",8:"H",9:"I",10:"J",11:"K"}
 for i in range(a,0,-1):
     for j in range(1,i+1):
-        print(j,end="")
+        print(d[i],end=" ")
     print()
