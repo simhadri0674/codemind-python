0b0f9215d7fd6508beac067ec736cbe33030ef56
Simhadri surisetti <109957109+simhadri0674@users.noreply.github.com>
2022-11-01 07:04:55 +0530

    def fun(a):     c=0     for i in a:         c +=i     return c a,b=map(int,input().split()) l=[] for i in range(a):     k=list(map(int,input().split()))     l.append(k) x=[] for j in l:     x.append(fun(j)) print(max(x))

---
+++ Maximum_sum_of_row_wise_sum_of_a_matrix.py
@@ -0,0 +1,14 @@
+def fun(a):
+    c=0
+    for i in a:
+        c +=i
+    return c
+a,b=map(int,input().split())
+l=[]
+for i in range(a):
+    k=list(map(int,input().split()))
+    l.append(k)
+x=[]
+for j in l:
+    x.append(fun(j))
+print(max(x))    
